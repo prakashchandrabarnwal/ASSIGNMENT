c9903684168956375b8d52e6d089c6b9094120a5
prakashchandrabarnwal <barnwalbikky@gmail.com>
2016-07-12 10:12:08 +0530

    updated

---
+++ javascript/hello.js
@@ -0,0 +1,53 @@
+/*var obj="Prakashchandrvbarnwal";
+length=obj.length;
+
+obj=obj.replace("v","a");
+obj=obj.toUpperCase();
+obj=obj.toLowerCase();
+alert(obj);
+*/
+
+/*var person=new Object();
+person.firstname="Prakash";
+person.lastName="Barnwal";
+person.fullName =function(){
+return this.firstname+" "+this.lastName;
+};
+alert(person.fullName());
+
+*/
+/*var person={
+	firstname:"Prakash",
+	lastName:"Chandra",
+	fullName:function(){
+		return this.firstname+" "+this.lastName;
+	}
+}
+alert(person.fullName());
+*/
+
+/*var foo1=["chandra","barnwal"];
+    
+
+var reversed=foo1.reverse();
+var joined=foo1.join(" . ");
+var sorted=foo1.sort();
+alert(reversed);
+alert(joined);
+alert(sorted);
+*/
+
+var foo="hello, window1 ";
+window.alert(window.foo);
+window.parseInt("3");
+
+var bar=function(){
+	var hello="hello function";
+	alert(window.hello);
+};
+
+bar();
+
+
+
+
